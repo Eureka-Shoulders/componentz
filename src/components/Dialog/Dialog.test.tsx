f1451c5de55d967d455650f1d11f0255b9403cd2
Gabriel Silva <devzgabriel@gmail.com>
2021-12-10 16:24:13 -0300

    remove warnigs

--- src/components/Dialog/Dialog.test.tsx
+++ src/components/Dialog/Dialog.test.tsx
@@ -1,17 +1,12 @@
 import 'reflect-metadata';
 
-import TYPES from '../../containers/global.types';
 import { UIStoreType } from '@stores/types';
-import {
-  getByTestId,
-  render,
-  RenderResult,
-  screen,
-} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Dialog, globalContainer } from 'index';
 import { Container } from 'inversify';
 import { Provider } from 'inversify-react';
 import React from 'react';
+import TYPES from '../../containers/global.types';
 
 describe('Dialog', () => {
   let onRejectSpy: jest.Mock;
