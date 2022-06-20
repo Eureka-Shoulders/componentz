fe00a437f38f72db791d1f68a8513e656c9b4d4b
Sampaio Leal <samp14hd@gmail.com>
2022-06-20 14:40:44 -0300

    fix: exporting types from index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 export { default as AppBar } from './AppBar';
+export * from './AppBar';
 export { default as Breadcrumb } from './Breadcrumbs';
+export * from './Breadcrumbs';
 export { default as Dialog } from './Dialog';
 export { default as Snackbar } from './Snackbar';
 export * from './Inputs';
