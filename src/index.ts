6819a040e2ff259b8cf24496620fc171642a257f
Sampaio Leal <samp14hd@gmail.com>
2022-06-20 13:52:14 -0300

    fix: export types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 // Components
 export * from './components';
 
+// Stores
+export * from './stores';
+
 // Hooks
 export { default as useUIStore } from './hooks/useUIStore';
 
 // Containers
 export { default as globalContainer } from './containers/global.inversify';
+export { default as globalBindings } from './containers/global.bindings';
